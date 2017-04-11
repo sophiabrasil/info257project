d5595f9acc58052059c2c8c23cdd29cd1d780d84
magik4rp <notfelineit@berkeley.edu>
2017-04-11 13:03:29 -0700

    Frances: Added some template files and routing

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template("home.html")
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
