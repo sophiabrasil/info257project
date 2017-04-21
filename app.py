f4b4ba5453e6aefd095e761bfd746a89a0d9cccc
jmlwang <jmlwang@berkeley.edu>
2017-04-21 14:50:57 -0700

    Jocelyns app

--- app.py
+++ app.py
@@ -1,11 +1,46 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template("home.html")
+import sqlite3 as lite
+import sys
+ 
+@app.route("/")
+def view_all_books():
+	
+	con = lite.connect("books.db")
+	cur = con.cursor()
+	cur.execute("select id, title, author from books")
+	rows = cur.fetchall()
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('page_not_found.html'), 404
+	return render_template("index.html", **locals())
+
+@app.route("/addbook", methods=["GET", "POST"])
+def add_book():
+
+	if request.method == "GET":
+		return render_template("addbook.html", **locals())
+
+	else:
+		title = request.form["title"]
+		author = request.form["author"]
+
+		con = lite.connect("books.db")
+		with con:
+			cur = con.cursor()
+			cur.execute("insert into Books (title, author) values ('{}', '{}')".format(title, author))
+
+		return redirect("/")
+
+
+@app.route("/book/<int:id>")
+def get_book(id):
+
+	con = lite.connect("books.db")
+	cur = con.cursor()
+	cur.execute("select id, title, author from books where id = " + str(id))
+	rows = cur.fetchall()
+
+	return render_template("viewbook.html", **locals())
+
+if __name__ == "__main__":
+    app.run()
